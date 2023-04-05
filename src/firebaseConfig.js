48581492d94d93da41c11002f62a7782ff013190
Luis Alberto Cabrera <cabreraluis1201@gmail.com>
2023-04-05 01:45:45 -0300

    .env

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAUuoZAd7LQpRQ4QXQOuQCngJ-26DtXSBo",
-  authDomain: "curso-react-coder-9947d.firebaseapp.com",
-  projectId: "curso-react-coder-9947d",
-  storageBucket: "curso-react-coder-9947d.appspot.com",
-  messagingSenderId: "297251025843",
-  appId: "1:297251025843:web:a978dfba6db80e2a5211c6"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
